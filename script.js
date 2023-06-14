d638308694f578dd9ff98e998693bdd8b62ab5bc
Kyky <126676754+kycimento@users.noreply.github.com>
2023-06-14 14:42:09 -0300

    up

---
+++ script.js
@@ -0,0 +1,15 @@
+var prova = parseFloat(prompt("Qual a nota da prova?"));
+var trabalho = parseFloat(prompt("Qual a nota do trabalho?"));
+var notaFinal = prova + trabalho;
+alert(notaFinal);
+
+
+
+/*var email = prompt("Digite seu e-mail", "");
+alert("O e-mail " + email + " foi coletado com sucesso.");
+var valor = confirm("Vamos juntos aprender a programar em JS");
+if (valor == true) {
+    alert("Parabéns, vamos lá!!!");
+} else {
+    alert("Opção errada, aperte F5 para recarregar a página.");
+}*/
